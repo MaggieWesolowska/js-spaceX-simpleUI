4a4e2e907489ffafe113eefeb606a1bdd37073e7
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-24 16:02:59 +0100

    added js function and styles

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const ROCKETS_URL = "https://api.spacexdata.com/v3/rockets";
 
-const fetchRockets = () => {
-  fetch(ROCKETS_URL)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
+const rocketsContainer = document.querySelector(".rockets");
+
+const fetchRockets = async () => {
+  const response = await fetch(ROCKETS_URL);
+  console.log(response);
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  const rockets = await response.json();
+  console.log(rockets);
+  return rockets;
 };
 
-fetchRockets();
+fetchRockets().then((rockets) => {
+  console.log(rockets);
+  rocketsContainer.innerHTML = rockets
+    .map((rocket) => {
+      return `<li>${rocket.rocket_name}</li>`;
+    })
+    .join("");
+});
