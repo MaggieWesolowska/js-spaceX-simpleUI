4159b2f72ce492b3a2540c2af04bb3a33fd6f19d
MaggieWesolowska <maggiewes07@gmail.com>
2023-03-24 18:09:58 +0100

    added js for single rocket

--- index.js
+++ index.js
@@ -14,10 +14,30 @@ const fetchRockets = async () => {
 };
 
 fetchRockets().then((rockets) => {
-  console.log(rockets);
   rocketsContainer.innerHTML = rockets
     .map((rocket) => {
-      return `<li>${rocket.rocket_name}</li>`;
+      return `<li data-rocket-id="${rocket.rocket_id}">${rocket.rocket_name}</li>`;
     })
     .join("");
 });
+
+const fetchSingleRocket = async (rocketId) => {
+  const response = await fetch(
+    `${ROCKETS_URL}/${rocketId}`
+  );
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  const singleRocket = await response.json();
+  return singleRocket;
+};
+
+rocketsContainer.addEventListener(
+  "click",
+  async (event) => {
+    console.log(event.target.dataset.rocketId);
+    const rocketId = event.target.dataset.rocketId;
+    const rocket = await fetchSingleRocket(rocketId);
+    console.log(rocket);
+  }
+);
